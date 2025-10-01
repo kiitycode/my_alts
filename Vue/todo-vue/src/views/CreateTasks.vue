7ad9b9da9b07b95205b7d35918678a572eb9a020
kiitycode <betosin99gmail.com>
2025-10-01 16:04:48 +0100

    feat: add views (Home, CreateTask, DetailPage, EditTask, Login, NotFound)

---
+++ Vue/todo-vue/src/views/CreateTasks.vue
@@ -0,0 +1,23 @@
+    <template>
+    <section>
+        <h1>Create Todo</h1>
+        <form @submit.prevent="create">
+        <input v-model="title" placeholder="title" />
+        <button :disabled="!title">Create</button>
+        </form>
+    </section>
+    </template>
+
+    <script setup>
+    import { ref } from 'vue'
+    import api from '../services/api'
+    import { useRouter } from 'vue-router'
+
+    const title = ref('')
+    const router = useRouter()
+
+    async function create() {
+    await api.createTask({ title: title.value, completed: false })
+    router.push({ name: 'Home' })
+    }
+    </script>
---
+++ Vue/todo-vue/src/views/Home.vue
@@ -0,0 +1,16 @@
+    <template>
+    <section>
+        <h1>Home</h1>
+        <p>Task list</p>
+        <ul>
+        <li v-for="t in todos" :key="t.id">
+            <router-link :to="{ name: 'DetailPage', params: { id: t.id }}">{{ t.title }}</router-link>
+        </li>
+        </ul>
+    </section>
+    </template>
+
+    <script setup>
+    import { useCachedTodos } from '../composables/useCachedTodos'
+    const { todos, loading } = useCachedTodos()
+    </script>
